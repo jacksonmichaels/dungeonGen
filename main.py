00a9235c30d1a53a1c2af38240316258758c9009
jacksonmichaels <jackson.michaels@mymail.champlain.edu>
2019-01-26 14:30:23 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from graphics import *
+
+win = GraphWin("Game")
+
+pt = Point(100, 50)
+
+pt.draw(win)
+
+cir = Circle(pt, 25)
+cir.draw(win)
+
+cir.setOutline('red')
+cir.setFill('blue')
+
+line = Line(pt, Point(150, 100))
+line.draw(win)
+
+rect = Rectangle(Point(20, 10), pt)
+rect.draw(win)
