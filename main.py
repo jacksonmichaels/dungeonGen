7b560e358470c033eafc9690b7c349f440aabce6
jacksonmichaels <jackson.michaels@mymail.champlain.edu>
2019-01-27 17:47:24 -0500

    done

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from graphics import *
 import os
 import random
+import sys
 #for Dir True = vertical False = horizontal
 
 
 #Constants:
-WINDOW_WIDTH = 1000
-WINDOW_HEIGHT = 500
-MIN_SIZE = 50000
+WINDOW_WIDTH = 2000
+WINDOW_HEIGHT = 1000
+MIN_SIZE = 1000
 
 
 
@@ -31,8 +32,11 @@ class Room:
 
         self.shape.move(x,y)
 
+    def getCenter(self):
+        return Point((self.topL.x + self.botR.x)/2, (self.topL.y + self.botR.y)/2)
+
 class Node:
-    def __init__(self, topLeft, botRight, left = None, right = None, dir = False, room = None):
+    def __init__(self, topLeft, botRight, left = None, right = None, dir = False, room = None, id=""):
         self.tl = topLeft
         self.br = botRight
 
@@ -46,6 +50,11 @@ class Node:
         self.parent = None
 
         self.room = room
+
+        self.childRooms = []
+        self.id = id
+
+        self.text = Text(Point(self.tl.x + 50, self.tl.y + 20), id)
 
     def getSize(self, mode = 'd'):
         wide = self.br.x - self.tl.x
@@ -61,28 +70,44 @@ class BSP:
         self.min = minSize
         self.win = window
 
-        self.head = Node(Point(0, 0), Point(WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.head = Node(Point(0, 0), Point(WINDOW_WIDTH, WINDOW_HEIGHT), id = "")
 
         self.rooms = 0
+
+
+    def makeMap(self):
+        self.generate(self.head)
+
+        self.makeRoomList(self.head)
+
+        self.connectRooms(self.head)
 
     def generate(self, node):
         (wide, tall) = node.getSize()
         if (node.dir):
-            left = Node(node.tl, Point(node.tl.x + int(wide/2), node.br.y), dir = not node.dir)
 
-            right = Node(Point(node.tl.x + int(wide / 2), node.tl.y), node.br, dir = not node.dir)
+            line = int(random.randrange(0, wide) * 0.4 + wide * 0.3)
+
+            left = Node(node.tl, Point(node.tl.x + line, node.br.y), dir = not node.dir, id = node.id + 'L ')
+
+            right = Node(Point(node.tl.x + line, node.tl.y), node.br, dir = not node.dir,  id = node.id + 'R ')
 
 
         else:
-            left = Node(node.tl, Point(node.br.x, node.tl.y + int(tall / 2)), dir = not node.dir)
 
-            right = Node(Point(node.tl.x, node.tl.y + int(tall / 2)), node.br, dir = not node.dir)
+            line = int(random.randrange(0, tall) * 0.4 + tall * 0.3)
+
+            left = Node(node.tl, Point(node.br.x, node.tl.y + line), dir = not node.dir,  id = node.id + 'L ')
+
+            right = Node(Point(node.tl.x, node.tl.y + line), node.br, dir = not node.dir,  id = node.id + 'R ')
+
 
         node.left = left
         node.right = right
 
         node.left.parent = node
         node.right.parent = node
+
 
 
         if (left.getSize('a') < MIN_SIZE):
@@ -93,7 +118,7 @@ class BSP:
 
             room.draw(self.win)
 
-            Rectangle(left.tl, left.br).draw(self.win)
+            Rectangle(node.left.tl, node.left.br).draw(self.win)
 
             node.left.room = room
             self.rooms += 1
@@ -110,7 +135,9 @@ class BSP:
 
             room.draw(self.win)
 
-            Rectangle(right.tl, right.br).draw(self.win)
+            Rectangle(node.right.tl, node.right.br).draw(self.win)
+
+
             node.right.room = room
             self.rooms += 1
 
@@ -118,26 +145,55 @@ class BSP:
             self.generate(node.right)
 
 
+    def makeRoomList(self, node):
+        if (node.room):
+            node.childRooms.append(node.room)
+
+        if (node.left):
+            node.childRooms += self.makeRoomList(node.left)
+        if (node.right):
+            node.childRooms += self.makeRoomList(node.right)
+
+        return node.childRooms
+
 
     def connectRooms(self, node):
-        if (node.left and node.right):
+        if (node.left):
             self.connectRooms(node.left)
+
+        if (node.right):
             self.connectRooms(node.right)
 
-        else:
-            node = node.parent
+        if (node.left and node.right):
 
-            lc = Point((node.left.room.topL.x + node.left.room.botR.x) / 2, (node.left.room.topL.y + node.left.room.botR.y) / 2)
-            rc = Point((node.right.room.topL.x + node.right.room.botR.x) / 2, (node.right.room.topL.y + node.right.room.botR.y) / 2)
+            pair = self.closestRooms(node.left.childRooms, node.right.childRooms)
 
-            link = Line(lc, rc)
+            link = Line(pair[0].getCenter(), pair[1].getCenter())
 
             link.setWidth(10)
 
             link.draw(self.win)
 
+    def closestRooms(self, aRooms, bRooms):
+        minDist = max(WINDOW_WIDTH, WINDOW_HEIGHT)
+        minPair = None
 
+        for aRoom in aRooms:
+            for bRoom in bRooms:
+                dist = getDist(aRoom.getCenter(), bRoom.getCenter())
+                if (dist < minDist):
+                    minDist = dist
+                    minPair = (aRoom, bRoom)
 
+        return minPair
+
+def getDist(pointA, pointB):
+    xdif = (pointA.x - pointB.x)**2
+    ydif = (pointA.y - pointB.y)**2
+
+    hyp = (xdif + ydif)**0.5
+
+    return hyp
 
 
 def addPoints(a, b):
@@ -145,7 +201,8 @@ def addPoints(a, b):
 
     return ret
 
-def randRoom(width, height, minDem = 50):
+def randRoom(width, height):
+    minDem = int(min(width, height) / 2)
     tl = Point(random.randrange(width - minDem), random.randrange(height - minDem))
 
     br = Point(random.randrange(tl.x + minDem, width), random.randrange(tl.y + minDem, height))
@@ -156,13 +213,9 @@ def randRoom(width, height, minDem = 50):
 
 win = GraphWin("Game", WINDOW_WIDTH, WINDOW_HEIGHT)
 
-
 tree = BSP(MIN_SIZE, win)
 
-tree.generate(tree.head)
-
-tree.connectRooms(tree.head)
-
+tree.makeMap()
 
 win.getMouse()
 
